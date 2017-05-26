1d5f90976b99a168f1409b121ef41ed7ba78579b
djedynak259 <dajed@outlook.com>
2017-05-25 21:01:44 -0700

    mor eedits

--- src/js/client.js
+++ src/js/client.js
@@ -1,1 +1,14 @@
-require('./module.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Layout extends React.Component {
+	render () {
+		return (
+				<h1>It Work</h1>
+			);
+	}
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<Layout/>, app);

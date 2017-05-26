91deb19d881c3cbc30722493c63d47e51011411f
djedynak259 <dajed@outlook.com>
2017-05-26 14:44:34 -0700

    browser router

--- src/js/client.js
+++ src/js/client.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Layout from './components/Layout';
 
 const app = document.getElementById('app');
 
 
 ReactDOM.render((
-	<HashRouter>
+	<BrowserRouter>
 		<Route path ="/" component={Layout}>
 		</Route>
-	</HashRouter>
+	</BrowserRouter>
 ),app);
 	
 

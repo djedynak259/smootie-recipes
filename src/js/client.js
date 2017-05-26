4f44e1401b1025602f88cf2a7a7169ee48770744
djedynak259 <dajed@outlook.com>
2017-05-26 14:42:07 -0700

    Router Added

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {HashRouter, Route} from 'react-router-dom';
 import Layout from './components/Layout';
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app); 
+
+
+ReactDOM.render((
+	<HashRouter>
+		<Route path ="/" component={Layout}>
+		</Route>
+	</HashRouter>
+),app);
+	
+

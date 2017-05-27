462c5b9937141f6e103a0b48fb1ad33f713c1c95
djedynak259 <dajed@outlook.com>
2017-05-27 10:27:59 -0700

    edits

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
 import Layout from './components/Layout';
 import Featured from './pages/Featured';
 import Archived from './pages/Archived';
@@ -11,8 +12,10 @@ const app = document.getElementById('app');
 
 ReactDOM.render((
 	<BrowserRouter>
-		<Route path ="/" component={Layout}>
-		</Route>
+		<Route path ="/" component={Layout}></Route>
+		<Route path ="Featured" component={Featured}></Route>
+		<Route path ="Archived" component={Archived}></Route>
+		<Route path ="Settings" component={Settings}></Route>
 	</BrowserRouter>
 ),app);
 	
